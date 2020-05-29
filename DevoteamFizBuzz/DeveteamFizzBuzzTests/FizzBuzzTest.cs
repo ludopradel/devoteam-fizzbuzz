49748ff775ed7adec4ac1be0667d64f6dcd20856
Noureddine BACCAR <noureddine.baccar@outlook.fr>
2020-05-29 10:53:30 +0200

    TDD - First Red Test

---
+++ DevoteamFizBuzz/DeveteamFizzBuzzTests/FizzBuzzTest.cs
@@ -0,0 +1,19 @@
+﻿using DevoteamFizBuzz;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeveteamFizzBuzzTests
+{
+    [TestFixture]
+    class FizzBuzzTest
+    {
+        [Test]
+        public void ShouldReturn1When1()
+        {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            Assert.That("1".Equals(fizzBuzz.GetTextFromNumber(1)));
+        }
+    }
+}
---
+++ DevoteamFizBuzz/DevoteamFizBuzz/FizzBuzz.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevoteamFizBuzz
+{
+    public class FizzBuzz
+    {
+        public object GetTextFromNumber(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
